c663d9f4f27505f864ddeffb3f9a66d5165afd2a
hyl1995 <592443616@qq.com>
2019-05-07 09:08:49 +0800

    new project

---
+++ provider/src/main/java/com/hyl/provider/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.hyl.provider;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProviderApplication.class);
+    }
+
+}
